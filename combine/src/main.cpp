06ba81e447dd91b0f7dc5637e78524a8dfcc1e36
danieldorel <73711681+danieldorel@users.noreply.github.com>
2020-12-14 21:27:29 -0500

    Add files via upload

--- combine/src/main.cpp
+++ combine/src/main.cpp
@@ -1,3 +1,4 @@
+
 #include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
 #include <DHT.h>
@@ -35,10 +36,10 @@ int counter=0;
 
 #define COLUMS           16
 #define ROWS             2
-#define DHTPIN 4 
-#define DHTTYPE DHT11 
+#define DHTPIN 4
+#define DHTTYPE DHT11
 #define LCD_SPACE_SYMBOL 0x20  //space symbol from the LCD ROM, see p.9 of GDM2004D datasheet
-const int oneWireBus = 23;  
+const int oneWireBus = 23;
 
 #define TdsSensorPin 14
 #define VREF 5.0 // analog reference voltage(Volt) of the ADC
@@ -46,12 +47,12 @@ const int oneWireBus = 23;
 int analogBuffer[SCOUNT]; // store the analog value in the array, read from ADC
 int analogBufferTemp[SCOUNT];
 int analogBufferIndex = 0,copyIndex = 0;
-float averageVoltage = 0,tdsValue = 0,temperature = 25;  
+float averageVoltage = 0,tdsValue = 0,temperature = 25;
 
 
 
 const char* ssid = "ESP32";  // Enter SSID here
-const char* password = "12345678"; 
+const char* password = "12345678";
 IPAddress local_ip(192,168,1,1);
 IPAddress gateway(192,168,1,1);
 IPAddress subnet(255,255,255,0);
@@ -68,8 +69,8 @@ bool LED2status = LOW;
 
 OneWire oneWire(oneWireBus);
 DallasTemperature sensors(&oneWire);
-LiquidCrystal_I2C lcd(PCF8574_ADDR_A21_A11_A01, 4, 5, 6, 16, 11, 12, 13, 14, POSITIVE); 
-DHT dht(DHTPIN, DHTTYPE); 
+LiquidCrystal_I2C lcd(PCF8574_ADDR_A21_A11_A01, 4, 5, 6, 16, 11, 12, 13, 14, POSITIVE);
+DHT dht(DHTPIN, DHTTYPE);
 
 
 
@@ -80,6 +81,9 @@ String SendHTML(uint8_t led1stat,uint8_t led2stat){
   ptr +="<title>LED Control</title>\n";
   ptr +="<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
   ptr +="body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;} h3 {color: #444444;margin-bottom: 50px;}\n";
+  ptr +="table {border-collapse: collapse; border: 1px solid black; width: 100%;}";
+  ptr +="th_1 {background-color: #3498db; color: #ffff;border: 1px solid black;}";
+  ptr +="th_2 {1px solid black;}";
   ptr +=".button {display: block;width: 80px;background-color: #3498db;border: none;color: white;padding: 13px 30px;text-decoration: none;font-size: 25px;margin: 0px auto 35px;cursor: pointer;border-radius: 4px;}\n";
   ptr +=".button-on {background-color: #3498db;}\n";
   ptr +=".button-on:active {background-color: #2980b9;}\n";
@@ -93,9 +97,9 @@ String SendHTML(uint8_t led1stat,uint8_t led2stat){
   ptr +="<body>\n";
   ptr +="<h1>ESP32 Web Server</h1>\n";
   ptr +="<h3>Using Access Point(AP) Mode</h3>\n";
-  
-  
-  
+
+
+
    if(led1stat)
   {ptr +="<p>LED1 Status: ON</p><a class=\"button button-off\" href=\"/led1off\">OFF</a>\n";}
   else
@@ -105,10 +109,13 @@ String SendHTML(uint8_t led1stat,uint8_t led2stat){
   {ptr +="<p>LED2 Status: ON</p><a class=\"button button-off\" href=\"/led2off\">OFF</a>\n";}
   else
   {ptr +="<p>LED2 Status: OFF</p><a class=\"button button-on\" href=\"/led2on\">ON</a>\n";}
-  
-  ptr +="<p>Sonar distance</p><a class=\"button button-test\" href=\"/led2off\">OFF</a>";
-  //ptr +=test;/////
-  
+
+  ptr += "<table class=\"table\"> <th class=\"th_1\">Water Temperature</th> <th class=\"th_1\">Air Temperature</th> <th class=\"th_1\">Air Humidity</th> <th class=\"th_1\">Water Level</th> </tr>";
+  ptr += "<tr> <td>"; ptr+=intw[0]; ptr+="</td>"; ptr += "<td>"; ptr+=inta1[0]; ptr+="</td>"; ptr += "<td>"; ptr+=inta[0]; ptr+="</td>";ptr += "<td>"; ptr+=intd[0]; ptr+="</td> </tr>";
+  ptr += "<tr> <td>"; ptr+=intw[1]; ptr+="</td>"; ptr += "<td>"; ptr+=inta1[1]; ptr+="</td>"; ptr += "<td>"; ptr+=inta[1]; ptr+="</td>";ptr += "<td>"; ptr+=intd[1]; ptr+="</td> </tr>";
+  ptr += "<tr> <td>"; ptr+=intw[2]; ptr+="</td>"; ptr += "<td>"; ptr+=inta1[2]; ptr+="</td>"; ptr += "<td>"; ptr+=inta[2]; ptr+="</td>";ptr += "<td>"; ptr+=intd[2]; ptr+="</td> </tr>";
+  ptr += "<tr> <td>"; ptr+=intw[3]; ptr+="</td>"; ptr += "<td>"; ptr+=inta1[3]; ptr+="</td>"; ptr += "<td>"; ptr+=inta[3]; ptr+="</td>";ptr += "<td>"; ptr+=intd[3]; ptr+="</td> </tr>";
+  ptr += "<tr> <td>"; ptr+=intw[4]; ptr+="</td>"; ptr += "<td>"; ptr+=inta1[4]; ptr+="</td>"; ptr += "<td>"; ptr+=inta[4]; ptr+="</td>";ptr += "<td>"; ptr+=intd[4]; ptr+="</td> </tr>";
   ptr +="</body>\n";
   ptr +="</html>\n";
   return ptr;
@@ -117,31 +124,31 @@ void handle_OnConnect() {
   LED1status = LOW;
   LED2status = LOW;
   Serial.println("GPIO4 Status: OFF | GPIO5 Status: OFF");
-  server.send(200, "text/html", SendHTML(LED1status,LED2status)); 
+  server.send(200, "text/html", SendHTML(LED1status,LED2status));
 }
 
 void handle_led1on() {
   LED1status = HIGH;
   Serial.println("GPIO4 Status: ON");
-  server.send(200, "text/html", SendHTML(true,LED2status)); 
+  server.send(200, "text/html", SendHTML(true,LED2status));
 }
 
 void handle_led1off() {
   LED1status = LOW;
   Serial.println("GPIO4 Status: OFF");
-  server.send(200, "text/html", SendHTML(false,LED2status)); 
+  server.send(200, "text/html", SendHTML(false,LED2status));
 }
 
 void handle_led2on() {
   LED2status = HIGH;
   Serial.println("GPIO5 Status: ON");
-  server.send(200, "text/html", SendHTML(LED1status,true)); 
+  server.send(200, "text/html", SendHTML(LED1status,true));
 }
 
 void handle_led2off() {
   LED2status = LOW;
   Serial.println("GPIO5 Status: OFF");
-  server.send(200, "text/html", SendHTML(LED1status,false)); 
+  server.send(200, "text/html", SendHTML(LED1status,false));
 }
 
 void handle_NotFound(){
@@ -174,14 +181,14 @@ void setup(){
   WiFi.softAP(ssid, password);
   WiFi.softAPConfig(local_ip, gateway, subnet);
   delay(100);
-  
+
   server.on("/", handle_OnConnect);
   server.on("/led1on", handle_led1on);
   server.on("/led1off", handle_led1off);
   server.on("/led2on", handle_led2on);
   server.on("/led2off", handle_led2off);
   server.onNotFound(handle_NotFound);
-  
+
   server.begin();
   Serial.println("HTTP server started");
 }
@@ -222,12 +229,12 @@ void displayWaterTemp(float temperatureF){
     lcd.print("Error!");
   lcd.setCursor(0,1);
   lcd.print("Water temp high");
-  digitalWrite(26, HIGH);/////
+  digitalWrite(13, HIGH);/////
   }
   else{
   lcd.print("Water temp ");
     lcd.print(temperatureF);
-    digitalWrite(26, LOW);/////
+    digitalWrite(13 , LOW);/////
   }
   //delay(3000);/////
   //lcd.clear();
@@ -246,7 +253,7 @@ void displayDHT(float h, float f){
     lcd.print(h);
     lcd.print("%");
   }
-  
+
   lcd.setCursor(0,1); //air temp display
   if(f < airTempLowParam){
   lcd.print("Error! AirTemp");
@@ -255,7 +262,7 @@ void displayDHT(float h, float f){
     lcd.print("Error! AirTemp");
   }
   else{
-  lcd.print("Air Temp "); 
+  lcd.print("Air Temp ");
     lcd.print(f);
     lcd.print("F");
   }
@@ -320,43 +327,49 @@ tdsValue=(133.42*compensationVolatge*compensationVolatge*compensationVolatge - 2
   digitalWrite(trigPin, LOW);
   pinMode(echoPin, INPUT);
   duration = pulseIn(echoPin, HIGH);
-  cm = (duration/2) / 29.1;     
+  cm = (duration/2) / 29.1;
   inches = (duration/2) / 74;
-  if(counter<5){
+  if(counter<2){
     lcd.clear();
   }
-  if(counter<25){
+  if(counter<22){
   displaySonar(inches);
   }
   //delay(5000);
   //lcd.clear();
+  if(counter==1){
   intd.push_back(inches);/////
+  }
   
   float h = dht.readHumidity();//DHT temp humid
   float f = dht.readTemperature(true);
   //delay(5000);
   //lcd.clear();
-  if(counter>25&&counter<30){
+  if(counter>22&&counter<24){
     lcd.clear();
   }
-  if(counter>30&&counter<50){
+  if(counter>24&&counter<44){
     displayDHT(h, f);
   }
+  if(counter==1){
   inta.push_back(h);/////
   inta1.push_back(f);/////
-
+  }
+  
   sensors.requestTemperatures();//Water temp
   float temperatureF = sensors.getTempFByIndex(0);
-  if(counter>50&&counter<55){
+  if(counter>44&&counter<46){
     lcd.clear();
   }
-  if(counter>55&&counter<75){
+  if(counter>46&&counter<66){
     displayWaterTemp(temperatureF);
   }
   //delay(5000);
   //lcd.clear();
+  if(counter==1){
   intw.push_back(temperatureF);/////
-  if(counter>75){
+  }
+  if(counter>66){
     counter=0;
   }
 
@@ -388,7 +401,7 @@ tdsValue=(133.42*compensationVolatge*compensationVolatge*compensationVolatge - 2
   {digitalWrite(LED1pin, HIGH);}
   else
   {digitalWrite(LED1pin, LOW);}
-  
+
   if(LED2status)
   {digitalWrite(LED2pin, HIGH);}
   else
