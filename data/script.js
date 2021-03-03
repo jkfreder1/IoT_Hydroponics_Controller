35b18803a89369c93e796af7a156dd532292d0ba
Liam Donovan <ltdonova@ncsu.edu>
2021-03-02 21:53:05 -0500

    added html, css, and JS

---
+++ data/script.js
@@ -0,0 +1,6 @@
+function logoutButton() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "/logout", true);
+    xhr.send();
+    setTimeout(function(){ window.open("/logged-out","_self"); }, 1000);
+}
