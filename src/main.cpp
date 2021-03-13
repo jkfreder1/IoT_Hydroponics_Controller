aedd635e268f0c36f42c5245375dfef9305e37ba
Liam Donovan <ltdonova@ncsu.edu>
2021-03-13 18:58:14 -0500

    moved things

--- src/main.cpp
+++ src/main.cpp
@@ -54,55 +54,13 @@ char msg[50];
 int value = 0;
 float temperatureMQTT = 0;
 float humidityMQTT = 0;
-/*void setup_wifi() {
-  delay(10);
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
 
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-}*/
-void callback(char* topic, byte* message, unsigned int length) {
-  Serial.print("Message arrived on topic: ");
-  Serial.print(topic);
-  Serial.print(". Message: ");
-  String messageTemp;
-  
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)message[i]);
-    messageTemp += (char)message[i];
-  }
-  Serial.println();
-
-  // Feel free to add more if statements to control more GPIOs with MQTT
-
-  // If a message is received on the topic esp32/output, you check if the message is either "on" or "off". 
-  // Changes the output state according to the message
-  if (String(topic) == "esp32/output") {
-    Serial.print("Changing output to ");
-    if(messageTemp == "on"){
-      Serial.println("on");
-      //digitalWrite(ledPin, HIGH);
-    }
-    else if(messageTemp == "off"){
-      Serial.println("off");
-      //digitalWrite(ledPin, LOW);
-    }
-  }
-}
-
-
+AsyncWebServer server(80);////////new web server
+DNSServer dns;
+const int ledPin = 2;
+String ledState;
+const char* http_username = "admin";
+const char* http_password = "admin";
 
 #define COLUMS           16
 #define ROWS             2
@@ -140,6 +98,40 @@ bool LED1status = LOW;
 
 uint8_t LED2pin = 27;
 bool LED2status = LOW;
+
+
+
+void callback(char* topic, byte* message, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+  
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
+
+  // Feel free to add more if statements to control more GPIOs with MQTT
+
+  // If a message is received on the topic esp32/output, you check if the message is either "on" or "off". 
+  // Changes the output state according to the message
+  if (String(topic) == "esp32/output") {
+    Serial.print("Changing output to ");
+    if(messageTemp == "on"){
+      Serial.println("on");
+      //digitalWrite(ledPin, HIGH);
+    }
+    else if(messageTemp == "off"){
+      Serial.println("off");
+      //digitalWrite(ledPin, LOW);
+    }
+  }
+}
+
+
+
 
 
 OneWire oneWire(oneWireBus);
@@ -274,12 +266,7 @@ void button3ISR(){
 
 
 
-AsyncWebServer server(80);////////new web server
-DNSServer dns;
-const int ledPin = 2;
-String ledState;
-const char* http_username = "admin";
-const char* http_password = "admin";
+
 
 String readBME280Humidity() {
   float h = 65 + rand() % (( 85 + 1 ) -65);
@@ -326,6 +313,13 @@ String processor(const String& var){
     if(!request->authenticate(http_username, http_password))
       return request->requestAuthentication();
     request->send(SPIFFS, "/script.js", "text/javascript");
+  });
+  // Route to load data.json file 
+  server.on("/data.json", HTTP_GET, [](AsyncWebServerRequest *request){
+    if(!request->authenticate(http_username, http_password))
+      return request->requestAuthentication();
+    request->send(SPIFFS, "/data.json", "text/javascript");
+
   });
   // Route to set GPIO to HIGH
   server.on("/on", HTTP_GET, [](AsyncWebServerRequest *request){
